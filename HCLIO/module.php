1ee5a035efad4438d46f3336fcbaf097f4adcb0a
root <root@raspberrypi>
2020-05-01 13:55:38 +0200

    SendDataToChildren in Funktion ForwardData eingefügt

--- HCLIO/module.php
+++ HCLIO/module.php
@@ -26,6 +26,8 @@
 		{
 			$data = json_decode($JSONString);
 			IPS_LogMessage("IO FRWD", utf8_decode($data->Buffer));
+			$Text = 'Antwort vom IO';
+			$this->SendDataToChildren(json_encode(Array("DataID" => "{D443C558-D963-4B2A-D017-B97CF576CB3A}", "Buffer" => $Text)));
 		}
 
 		public function Send(string $Text)
